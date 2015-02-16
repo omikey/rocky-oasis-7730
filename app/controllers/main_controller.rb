74560f2353cf0d1319193891e42ca7ff82c77a81
Michael O'Riley <michael@oriley.org>
2015-02-16 15:25:31 -0800

    Rotated chart to match up with the design plans.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -6,7 +6,7 @@ class MainController < ApplicationController
     @listening = []
     4.times do |k|
       @listening.push({title:"Part #{(k+1).to_s}",
-                       score: 50 + rand(50),
+                       score: 50 + rand(51),
                        mypace: "#{(2 + rand(2)).to_s}s",
                        yourpace: "#{(2 + rand(2)).to_s}s"})
     end
@@ -14,12 +14,12 @@ class MainController < ApplicationController
     @reading = []
     3.times do |k|
       @reading.push({title:"Part #{(k+5).to_s}",
-                     score: 50 + rand(50),
-                     mypace: "#{(2 + rand(2)).to_s}s",
-                     yourpace: "#{(2 + rand(2)).to_s}s"})
+                     score: 50 + rand(51),
+                     mypace: "#{(25 + rand(56)).to_s}s",
+                     yourpace: "#{(25 + rand(56)).to_s}s"})
     end
 
-    rand = rand(50)
+    rand = rand(51)
     @readingWPM = {total:100 + rand,percent:(((100+rand)/1.5) + 0.5).to_i}
 
     rand = rand(4000)
@@ -29,7 +29,7 @@ class MainController < ApplicationController
     @improvement = []
 
     7.times do |k|
-      @improvement.push(50 + rand(50))
+      @improvement.push(50 + rand(51))
     end
     render json: {listening: @listening, reading: @reading,
                   readingWPM: @readingWPM, vocabulary: @vocabulary,
