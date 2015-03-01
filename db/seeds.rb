9b7a3fe39e3602518f4f73eefe8bc10ba4339272
Michael O'Riley <michael@oriley.org>
2015-03-01 14:15:12 -0800

    Fixed redirect after submit/delete/edit.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 #encoding: utf-8
 
-Forum.find_or_create_by(title: 'Hey Roger')
-
-Query.find_or_create_by(title: 'Check This Out', forum_id: 1, user_id: 1)
-
-Post.find_or_create_by(message: 'I made a dang message board', query_id: 1, user_id: 1)
+unless Forum.any?
+  Forum.new(title: 'Hey Roger').save
+  Query.new(title: 'Check This Out', forum_id: 1, user_id: 1).save
+  Post.new(message: 'I made a dang message board', query_id: 1, user_id: 1).save
+end
